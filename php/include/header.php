31bdad408db836e8ac814420401de9a17091ee93
Casey Kruijer <251570@cursist.rockopnh.nl>
2019-11-27 15:13:56 +0100

    navigatie goed en de header

---
+++ php/include/header.php
@@ -0,0 +1,9 @@
+<head>
+	<title>MA Twente</title>
+	<meta charset="UTF-8">
+	<meta name="description" content="dit is het appotheek van meneer schut">
+	<meta name="keywords" content="schut, appotheek, medicijnen, ">
+	<meta name="author" content="Casey Kruijer">
+	<meta name="viewport"content="width=device-witdth, initial-scale=1-0">	
+	<link rel="stylesheet" href="../css/navigatie.css">
+</head>
