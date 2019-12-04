68935d12a4b2d03d0996c156ac08d93036ea1dc6
Casey Kruijer <251570@cursist.rockopnh.nl>
2019-12-04 13:24:12 +0100

    include navigatie + header af en klaar

---
+++ include/header.php
@@ -0,0 +1,10 @@
+<head>
+	<title>MA Twente</title>
+	<meta charset="UTF-8">
+	<meta name="description" content="dit is het MA Twente project">
+	<meta name="keywords" content="aplicatie voor MA Twente ">
+	<meta name="author" content="Wesley van de Slikke,Bart Rooijakkers, Casey Kruijer">
+	<meta name="viewport"content="width=device-witdth, initial-scale=1-0">
+	<link rel="stylesheet" href="../css/style.css">
+	<link rel="stylesheet" href="../css/navigatie.css">
+</head>
---
+++ include/navigatie.php
@@ -0,0 +1,15 @@
+<div class="topnav">
+	<a href="home.html">Home</a>
+	<a href="info over mij.html">Info over mij</a>
+    <a href="hobby's.html">Hobby's</a>
+	<a href="cv.html">CV</a>
+	<a href="contact.html">Contact</a>
+</div>
+
+<div class="sidenav">
+	<a href="home.html">Home</a>
+	<a href="over ons.html">Over ons</a>
+	<a href="producten.html">Producten</a>
+	<a href="thema's.html">Thema's</a>
+	<a href="bestellencontact.html">contact bestellen</a>
+</div>	
