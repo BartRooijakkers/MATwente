8a10223ba676feec5d118c6721a61e805f25a97c
Bart <bart_rooyakkers@hotmail.com>
2019-12-04 22:25:47 +0100

    Twente.sql Geupdate, Experimental version
    Alles geimporteerd behalve configuraties,
    Misschien is incidenten niet goed

--- twente.sql
+++ twente.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Gegenereerd op: 04 dec 2019 om 16:43
+-- Gegenereerd op: 04 dec 2019 om 22:23
 -- Serverversie: 10.4.6-MariaDB
 -- PHP-versie: 7.3.9
 
@@ -100,6 +100,17 @@ CREATE TABLE `hardware` (
   `brand` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Gegevens worden geëxporteerd voor tabel `hardware`
+--
+
+INSERT INTO `hardware` (`hardwareID`, `model`, `type`, `brand`) VALUES
+(1, 'K280e', 'toetsenbord', 'Logitech'),
+(2, 'LGT-M90', 'muis', 'Logitech'),
+(3, '24MK400H-B', 'monitor', 'LG'),
+(4, 'HP800G1i7', 'desktop', 'HP'),
+(5, 'SM-T510NZKDPHN', 'tablet', 'Samsung');
+
 -- --------------------------------------------------------
 
 --
@@ -112,7 +123,7 @@ CREATE TABLE `incident` (
   `description` varchar(255) DEFAULT NULL,
   `shortDescription` varchar(255) DEFAULT NULL,
   `impact` int(3) DEFAULT NULL,
-  `time` int(3) DEFAULT NULL,
+  `time` float(3,2) DEFAULT NULL,
   `responsibleID` int(11) DEFAULT NULL,
   `cause` varchar(255) DEFAULT NULL,
   `solution` varchar(255) DEFAULT NULL,
@@ -120,6 +131,45 @@ CREATE TABLE `incident` (
   `date` timestamp NULL DEFAULT NULL,
   `type` int(3) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `incident`
+--
+
+INSERT INTO `incident` (`incidentID`, `statusID`, `description`, `shortDescription`, `impact`, `time`, `responsibleID`, `cause`, `solution`, `feedback`, `date`, `type`) VALUES
+(1, 3, 'Mark Barney weet na zijn vakantie zijn wachtwoord niet meer.', 'Wachtwoord vergeten', 1, 0.10, 1, 'Niet van toepassing', 'Wachtwoord gereset naar standaard waarden. Optie Change password at next logon aagezet.', 'Gebruiker gebeld met nieuw tijdelijk wachtwoord.', '2019-12-02 15:32:00', 1),
+(2, 2, 'Bestand staat niet meer in de map van het project. Bestand is vorige week dinsdag voor het laatst aangevuld en opgeslagen.', 'Bestand met planning van project is weg.', 2, 1.00, 1, 'Bestand gewist', 'Bestand teruggehaald van restore.', 'Gemeld dat bestand terug staat.', '2019-12-02 15:32:00', 1),
+(3, 1, 'E-mailomgeving is niet te benaderen door kantoor en buitendienst medewerkers.', 'E-mail niet beschikbaar', 80, 2.00, 2, 'DNS-configuratie fout Hosting provider', 'Provider gecontact. deze hebben de fout in DNS-server opgelost. Aanpassing duurt 4 uur voordat deze overal doorwerkt.', 'Alle gebruikers een e-mail gestuurd dat het probleem opgelost is.', '2019-12-02 15:32:00', 1),
+(4, 4, 'Gebruiker wil een pagina in een Word document landscape afdrukken. Pagina\'s voor en na deze pagina dienen portrait te blijven.', 'Secties in Word', 1, 0.50, 1, 'Secties gebruiken', 'Met gebruiker meegekeken. de werking van secties uitgelegd en samen ingesteld voor het document.', 'Niet van toepassing.', '2019-12-02 15:32:00', 1),
+(5, 3, 'Als laptop opstart alleen drie piepjes te horen. Laptop doet verder niets.', 'Piepjes bij aanzetten laptop', 1, 1.50, 1, 'Geheugen vervangen', 'Geheugen blijkt defect. Geheugen vervangen. Geheugen wordt RMA gestuurd.', 'Gebruiker heeft laptop direct meegenomen.', '2019-12-02 15:32:00', 2),
+(6, 7, 'Gebruiker wilt zakeljke e-mail op privétablet', 'E-mail op privétablet', 1, 0.25, 1, 'Standaard instellingen', 'Telefonisch samen met de gebruiker tablet ingesteld.', 'Niet van toepassing.', '2019-12-03 15:32:00', 1),
+(7, 7, 'Laptop is gevallen. Er zit nu een breuk in scherm.', 'Breuk in laptopscherm', 1, 1.50, 1, 'Tijdelijk andere laptop', 'Data overgezet naar tijdelijke laptop. Deze laptop wordt opgestuurd naar leverancier voor reparatie.', 'Aan gebruiker gemeld dat nieuwe laptop opgehaald kan worden.', '2019-12-03 15:32:00', 2),
+(8, 3, 'Ik wil e-mailinstellingen van een extern e-mailadres op mijn tablet instellen. maar ik weet het wachtwoord niet meer. Account met wachtwoord zit nog wel op laptop van de zaak.', 'Wachtwoord omzetten', 1, 1.00, 1, 'Wachtwoord terug gezocht.', 'Met behulp van wachtwoord tool wachtwoord in Outlook zichtbaar gemaakt.', 'Wachtwoord telefonisch aan gebruiker doorgegeven.', '2019-12-03 15:32:00', 1),
+(9, 2, 'Alle buitendienstmedewerkers kunnen na de update niet meer inloggen op de urenadministratie.', 'Buitendienstmedewerkers geen toegang tot urenadministratie', 1, 3.00, 1, 'IP-routing niet goed', 'Route naar VPN server toegevoegd aan routing tabel van server.', 'Alle buitendienstmedewerkers gemaild.', '2019-12-04 15:32:00', 1),
+(10, 6, 'Gebruiker wil tijdens vakantie afwezigheidsassistent aanzetten.', 'Hoe stel ik de afwezigheidsassistent in?', 1, 0.25, 1, 'Bedrijfsbeleid uitgelegd', 'Uitgelegd aan gebruiker dat deze optie niet aanstaat in het systeem. Bedrijfsbeleid is om collega\'s tijdens de vakantie toegang te geven tot je mailbox zodat deze de mail kunnen bijhouden.', 'Niet van toepassing.', '2019-12-04 15:32:00', 1),
+(11, 3, 'Op de harddisk van mijn laptop heb ik per ongeluk een bestand verwijderd. Dit is een rapportage die ik nog niet had opgeslagen op de server. Hier zit drie weken werk in. Het is belangrijk dat deze terug komt.', 'per ongeluk een bestand verwijderd', 1, 3.00, 1, 'Bestand gerecoverd', 'Met tool bestand teruggehaald.', 'Bestand op server gezet en gebruiker gemaild.', '2019-12-04 15:32:00', 1),
+(12, 6, 'Pc is traag. Ook nadat door ICT-afdeling een nieuwe image geïnstalleerd is.', 'Pc reageert heel traag ook na herstarten', 1, 2.00, 1, 'BIOS-instellingen goed zetten', 'Na draaien van performace tool bleken BIOS-instellingen geheugen niet goed te staan.', 'Gebruiker laptop terug gegeven.', '2019-12-05 15:32:00', 2),
+(13, 3, 'Gebruiker is het wachtwoord van privélaptop vergeten. Graag wachtwoord verwijderen of instellen op ander wachtwoord.', 'Lokaal wachtwoord kwijt', 1, 1.50, 1, 'Wachtwoord teruggezocht', 'Met behulp van tool Windows wachtwoord achterhaald.', 'Wachtwoord telefonisch aan gebruiker doorgegeven.', '2019-12-05 15:32:00', 1),
+(14, 6, 'Bij opstarten internetbrowser worden advertenties getoond. Bij wegklikken komen er steeds meer advertenties.', 'Vreemde pagina\'s op internet', 1, 1.00, 1, 'Malware verwijderd', 'Malware van laptop verwijderd.', 'Gebruiker gemeld en komt laptop weer ophalen.', '2019-12-05 15:32:00', 1),
+(15, 7, 'Ik heb het idee dat iemand op afstand probeert in te loggen op mijn laptop. Is het mogelijk om hier iets van te achterhalen?', 'Iemand probeert in te loggen op laptop', 1, 0.50, 1, 'Logfiles bekeken', 'Remote log\'s bekeken en er probeert inderdaad iemand via remote desktop in te loggen. Remote desktop op laptop is nu uitgezet.', 'Telefonisch aan gebruiker doorgegeven.', '2019-12-06 15:32:00', 2),
+(16, 1, 'Buitendienstmedewerkers hebben geen toegang meer tot het netwerk.', 'Geen VPN-toegang', 40, 4.00, 1, 'Interface in router defect', 'WAN-interface van router die werkt als VPN-server defect. Reservekaart geplaatst. opnieuw geconfigureerd en getest.', 'Alle buitendienstmedewerkers gemaild.', '2019-12-06 15:32:00', 1),
+(17, 3, 'Er is een nieuwe medewerkster aangenomen voor projectplanning. Haar naam is Fee Willemse. Ze begint volgende week maandag. Graag account aanmaken met dezelfde rechten als die van haar collega Sigrid van der Wiel.', 'Nieuwe medewerker op projectplanning', 1, 0.25, 3, 'Niet van toepassing', 'Gebruiker is aangemaakt.', 'Mail met login gegevens verzonden aan hoofd van afdeling.', '2019-12-02 15:32:00', 1),
+(18, 3, 'Detail informatie: Na een half uur werken met de pc wordt het scherm zwart en slaat de pc uit. Na 5 minuten kan de pc weer gebruikt worden. maar werkt dan nog ongeveer een kwartier voordat hij weer uitvalt.', 'PC loopt soms vast', 1, 3.00, 3, 'CPU-koeling zal vol met stof.', 'Pc schoongemaakt en getest. Probleem lijkt verholpen.', 'Pc terug geplaatst en aan gebruiker gemeld.', '2019-12-02 15:32:00', 2),
+(19, 2, 'Alle pc\'s van het secretariaat geven aan dat ze niet verbonden zijn met een netwerk.', 'Secretariaat zonder netwerk', 6, 2.00, 3, 'Switch defect', 'Switch was defect. Deze is vervangen. Nieuwe switch is voorzien van de juiste configuratie-instellingen.', 'Bij afdeling langs gelopen om te vertellen dat het probleem verholpen is.', '2019-12-02 15:32:00', 2),
+(20, 3, 'Sarisa de Hoogt weet na vakantie haar wachtwoord niet meer.', 'Wachtwoord vergeten', 1, 0.10, 3, 'Niet van toepassing', 'Wachtwoord gereset naar standaard waarden. Optie Change password at next logon aagezet.', 'Gebruiker gebeld met nieuw tijdelijk wachtwoord.', '2019-12-02 15:32:00', 1),
+(21, 3, 'Bij het opstarten van de pc verschijnt de melding \"Boot failure. Reboot en Select proper Boot device or Insert Boot Media in selected Boot device\".', 'PC start niet meer op', 1, 1.50, 3, 'Defecte harddisk', 'Hardisk vervangen en standaard image terug gezet.', 'Persoonlijk aan gebuiker doorgegeven dat pc weer werkt.', '2019-12-03 15:32:00', 2),
+(22, 6, 'Printer geeft melding \"Engine Error please contact administrator\". Gebruikers printen nu op printer van financiële administratie.', 'Papier zit vast in printer', 15, 0.25, 4, 'Tandwiel defect', 'Bij leverancier aangemeld; wordt binnen 4 werkdagen vervangen.', 'Gebruikers gemaild dat de printer binnen 4 werkdagen gerepareerd wordt door de leverancier.', '2019-12-03 15:32:00', 2),
+(23, 6, 'Op alle kantoorwerkplekken verschijnt dagelijks meerdere malen de melding dat JAVA geupdate moet worden.', 'Java update melding', 40, 2.00, 3, 'Oude versie van JAVA was geïnstalleerd', 'Via AD nieuwe versie van JAVA uitgerold.', 'Niet direct teruggemeld aan gebruikers.', '2019-12-03 15:32:00', 1),
+(24, 6, 'Het benaderen van data vanaf de servers en vanaf het internet is traag. Het gaat wel. maar heel langzaam.', 'Netwerk is traag bij financiele administratie', 4, 3.00, 3, 'Configuratiefout in switch', 'Alle poorten op 1000 mbps full duplex gezet.', 'Oplossing aan afdelingshoofd gemeld.', '2019-12-03 15:32:00', 2),
+(25, 2, 'Er kunnen geen bestanden meer opgeslagen worden op de CAD-server. Het openen van bestanden gaat nog wel.', 'Geen oplag capaciteit meer op CAD-server', 6, 3.00, 3, 'Geen capaciteit meer op virtuele disken', 'Virtuele disken en partities vergroot.', 'Gemeld aan CAD-tekenaars dat er weer capaciteit is.', '2019-12-04 15:32:00', 1),
+(26, 7, 'Evert de Vrind. een medewerker van de CAD-afdeling. is op staande voet ontslagen. Zijn account moet per direct afgelosten worden.', 'Ontslagen medewerker CAD-afdeling', 1, 0.25, 3, 'Account disabelen', 'Account gedisabeld.', 'Aan HRM doorgegeven dat account uitstaat.', '2019-12-04 15:32:00', 1),
+(27, 7, 'Werkplek van secretariaat moet omgezet worden naar de afdeling HRM.', 'Verhuizen werkplek', 1, 2.00, 3, 'Niet van toepassing', 'Desktop verhuisd. nieuwe patch gemaakt en oude verwijderd. Patch documentatie is bijgewerkt.', 'Gebruiker ging direct op nieuwe plaats aan het werk.', '2019-12-04 15:32:00', 2),
+(28, 5, 'Gebruiker wilt PDF maken met spreadsheet. In die voettekst moet datum en versie vermeld worden.', 'Voettekst op spreadsheet PDF', 1, 1.00, 3, 'Voetteksten in Excel', 'Gebruiker uitgelegd hoe voetteksten in Excel werken. En verteld van de werking van CutePDF. Enkele malen het hele proces gezamenlijk doorlopen. Gebruiker vindt dit lastig.', 'Niet van toepassing', '2019-12-05 15:32:00', 1),
+(29, 1, 'Netwerk lijkt in zijn geheel niet meer te werken. ', 'Netwerk plat. ', 80, 2.50, 3, 'Patchfout', 'Patchfout heeft netwerkloop veroorzaakt.', 'Gemeld aan secretariaat. Die verzorgen communicatie naar rest van het bedrijf.', '2019-12-05 15:32:00', 1),
+(30, 2, 'In verband met defecte printer wilt gebruiker dat prints automatisch uit de printer van HRM komen.', 'Standaard printer', 1, 0.25, 3, 'Andere standaard printer', 'Remote HRM printer ingesteld als standaard.', 'Niet van toepassing', '2019-12-05 15:32:00', 2),
+(31, 3, 'Gebruiker kan niet inloggen op administratiepakket.', 'Administratie pakket niet toegangelijk', 1, 0.25, 3, 'Gebruiker aanmaken in administratiepakket', 'Gebuiker aangemaakt in administratiepakket. Gebruiker had nog geen account.', 'Aan hoofd van de afdeling gemeld dat de gebruiker kan inloggen.', '2019-12-06 15:32:00', 1),
+(32, 6, 'Bij opstarten internetbrowser worden advertenties getoond. Bij weg klikken komen er steeds meer advertenties.', 'Vreemde pagina\'s op internet', 1, 1.00, 3, 'Malware verwijderd.', 'Malware van laptop verwijderd.', 'Gebruiker gemeld en komt laptop weer ophalen.', '2019-12-06 15:32:00', 1),
+(33, 3, 'Is het wachtwoord van privélaptop vergeten. Graag wachtwoord verwijderen of instellen op ander wachtwoord.', 'Lokaal wachtwoord kwijt', 1, 1.50, 3, 'Wachtwoord terug gezocht.', 'Met behulp van tool Windows wachtwoord achterhaald.', 'Wachtwoord telefonisch aan gebruiker doorgegeven.', '2019-12-06 15:32:00', 1);
 
 -- --------------------------------------------------------
 
@@ -375,13 +425,13 @@ ALTER TABLE `departments`
 -- AUTO_INCREMENT voor een tabel `hardware`
 --
 ALTER TABLE `hardware`
-  MODIFY `hardwareID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `hardwareID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT voor een tabel `incident`
 --
 ALTER TABLE `incident`
-  MODIFY `incidentID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `incidentID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
 
 --
 -- AUTO_INCREMENT voor een tabel `responsible`
