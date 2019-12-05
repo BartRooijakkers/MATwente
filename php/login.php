f834fc9d5b00f22af34eed4a81fe22f2d36c361f
lil_albertus <bart_rooyakkers@hotmail.com>
2019-12-05 17:13:55 +0100

    Login pagina ingevoegd,
    Opmaak login pagina gemaakt, Formulier aangemaakt.

--- php/login.php
+++ php/login.php
@@ -1,5 +1,28 @@
 <!doctype html>
 <html lang="nl">
-	<body>	
-	</body>	
+<?php include('../include/header.php');?>
+	<body>
+
+
+				<div class="container">
+					<div class="login">
+						 <form action="login.php" method="post">
+				<br>
+
+					<label for="username"><b>E-mail</b></label><br>
+					<input type="text" placeholder="Vul uw E-mail in" name="username" required><br>
+
+					<label for="password"><b>Wachtwoord</b></label><br>
+					<input type="password" placeholder="Voer wachtwoord in" name="password" required><br>
+
+					<button type="submit" class="btn" name="login_btn">Login</button>
+					<br>
+
+			<label class="error"></label>
+			<br>
+			</div>
+		</form>
+		</div>
+
+	</body>
 </html>
