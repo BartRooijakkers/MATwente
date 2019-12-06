30bc410da4d8d247a0d22d02eba1dcc4dbc0a223
Bart <bart_rooyakkers@hotmail.com>
2019-12-06 11:55:57 +0100

    Added font
    Font: Lemonmilk geïmplementeerd

--- include/navigatie.php
+++ include/navigatie.php
@@ -1,7 +1,7 @@
 <div class="topnav">
-	<a href="configuratie.php">Configuratie</a>
+	<a href="configuraties.php">Configuratie</a>
     <a href="incidenten.php">Incidenten</a>
-	<a href="gerbuikers.php">Gerbuikers</a>
+	<a href="gebruikers.php">Gebruikers</a>
 	<a href="login.php">Login</a>
 </div>
 
@@ -10,4 +10,4 @@
 	<a href="gebruikertoevoegen.php">Gebruiker toevoegen</a>
 	<a href="incidentmeld.php">Incident Melden</a>
 	<a href="faq.php">Veel Gestelde Vragen</a>
-</div>	
+</div>
