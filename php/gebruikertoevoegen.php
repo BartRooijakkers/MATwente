d699be46a75c1d85b54631ec70761464cf1df4de
Casey Kruijer <251570@cursist.rockopnh.nl>
2019-12-11 14:05:29 +0100

    gerbuiker toevoegen opmaak

--- php/gebruikertoevoegen.php
+++ php/gebruikertoevoegen.php
@@ -3,8 +3,33 @@
  ?>
 <!doctype html>
 <html lang="nl">
-<?php include('../include/header.php');?>
+	<?php include('../include/header.php');?>
 <body>
 	<?php include('../include/navigatie.php');?>
-	</body>
+	<br>
+				<div class="container">
+					<div class="login">
+						 <form action="gebruikertoevoegen.php" method="post">
+				<br>
+					<label for="username"><b>Gebruiker</b></label><br>
+					<input type="text" placeholder="Vul uw Gebruikersnaam in" name="username" required><br>
+
+					<label for="password"><b>Wachtwoord</b></label><br>
+					<input type="password" placeholder="Vul uw wachtwoord in" name="password" required><br>
+					
+					<label for="password"><b>e-mail</b></label><br>
+					<input type="password" placeholder="Vul uw e-mail in" name="password" required><br>
+					
+					<label for="password"><b>Nummer</b></label><br>
+					<input type="password" placeholder="Vul uw nummer in" name="password" required><br>
+
+					<button type="submit" class="btn" name="login_btn">Aanmaken</button>
+					<br>
+
+			<label class="error"></label>
+			<br>
+			</div>
+		</form>
+		</div>
+</body>
 </html>
